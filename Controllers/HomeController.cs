674cf243aff3a969680734f50f020be8f253f957
mallorysmith64 <mallorysmith64@gmail.com>
2020-02-19 15:15:04 -0500

    files post

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,29 +1,29 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.IO;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace upload_files.Controllers
 {
     public class HomeController : Controller
     {
-        private Microsoft.AspNetCore.Hosting.IWebHostEnvironment _env;
-        public HomeController(Microsoft.AspNetCore.Hosting.IWebHostEnvironment env)
-        {
-            _env = env;
-        }
         public IActionResult Index() => View();
 
-        public IActionResult SingleFile(IFormFile file)
+    [HttpPost("UploadFiles")]
+        public static async Task<HttpResponseMessage> Post(string baseUrl, string xmlString)
         {
-            var dir = _env.ContentRootPath;
-
-            using (var fileStream = new FileStream(Path.Combine(dir, "file.png"), FileMode.Create, FileAccess.Write))
-        {
-            file.CopyTo(fileStream);
-        }
-            return RedirectToAction("Index");
+        using (var httpClient = new HttpClient())
+           {
+               var httpContent = new StringContent(xmlString, Encoding.UTF8, "text/xml");
+               httpContent.Headers.Add("SOAPAction", "http://LAPTOP-5R3N8CS8:9090");
+               System.Diagnostics.Debug.Write("async was invoked");
+               return await httpClient.PostAsync(baseUrl, httpContent);  
+           }
         }
     }
 }
