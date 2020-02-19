394c61eaaae3696f4c5e9c856cfd3bae102de9bd
mallorysmith64 <mallorysmith64@gmail.com>
2020-02-18 19:12:29 -0500

    single file uploads

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,35 +9,36 @@ namespace upload_files.Controllers
     public class HomeController : Controller
     {
         private Microsoft.AspNetCore.Hosting.IWebHostEnvironment _env;
-        string _incoming;
         public HomeController(Microsoft.AspNetCore.Hosting.IWebHostEnvironment env)
         {
             _env = env;
-            _incoming = Environment.GetEnvironmentVariable("INCOMING_DIR");
         }
         public IActionResult Index() => View();
 
         public IActionResult SingleFile(IFormFile file)
         {
-            string ts = DateTime.Now.ToString("yyyyMMddHHmmssFFF"); // case sensitive
+            var dir = _env.ContentRootPath;
 
-            using (var fileStream = new FileStream(Path.Combine(_incoming, $"incoming-{ts}.xml"), FileMode.Create, FileAccess.Write))
+            using (var fileStream = new FileStream(Path.Combine(dir, "file.png"), FileMode.Create, FileAccess.Write))
         {
             file.CopyTo(fileStream);
         }
             return RedirectToAction("Index");
         }
-    public IActionResult MultipleFiles(IEnumerable<IFormFile> files)
-    {
-            foreach (var file in files)
-            {
-                string ts = DateTime.Now.ToString("yyyyMMddHHmmssFFF"); // case sensitive
-                using (var fileStream = new FileStream(Path.Combine(_incoming, $"incoming-{ts}.xml"), FileMode.Create, FileAccess.Write))
-                {
-                    file.CopyTo(fileStream);
-                }
-            }
-            return RedirectToAction("Index");
-        }    
     }
 }
+//     public IActionResult MultipleFiles(IEnumerable<IFormFile> files)
+//     {
+//             int i = 0;
+//             foreach (var file in files)
+//             {
+//                 var dir = _env.ContentRootPath;
+//                 using (var fileStream = new FileStream(Path.Combine(dir, $"file{i++}.png"), FileMode.Create, FileAccess.Write))
+//                 {
+//                     file.CopyTo(fileStream);
+//                 }
+//             }
+//             return RedirectToAction("Index");
+//         }    
+//     }
+// }
--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export INCOMING_DIR="/mnt/c/Users/Mallory/Documents/final_project/In"
-# echo INCOMING_DIR=${INCOMING_DIR}
-# ls -l ${INCOMING_DIR}
-dotnet watch run
